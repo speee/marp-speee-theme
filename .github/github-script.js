b1583e45982eb8f3927955bf6c8fcabc8ee78425
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-09-24 08:59:17 +0900

    github scriptのステップへコミットコメント追加

--- .github/github-script.js
+++ .github/github-script.js
@@ -1,1 +1,8 @@
-module.exports = async ({ github, context, core }) => {};
+module.exports = async ({ github, context, core }) => {
+  github.repos.createCommitComment({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    commit_sha: context.sha,
+    body: "👋 Thanks for reporting!",
+  });
+};
