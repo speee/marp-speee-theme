7d3cc1278303a46f8df546c83dc3edf4e66682d4
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-09-25 02:37:19 +0900

    まだコメントされない理由がわからない

--- .github/github-script.js
+++ .github/github-script.js
@@ -1,10 +1,8 @@
 module.exports = async ({ github, context, core }) => {
-  var result = github.rest.repos.createCommitComment({
+  github.rest.repos.createCommitComment({
     owner: context.repo.owner,
     repo: context.repo.repo,
     commit_sha: context.sha,
     body: "👋 Thanks for reporting!",
   });
-  console.log("bbbbbbbbbbbbbbbbbb");
-  console.log(result);
 };
