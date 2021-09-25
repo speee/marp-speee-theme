2c39ef7ed4579c15ae37ae56104b589e1fd74774
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-09-25 10:42:14 +0900

    javascriptのテンプレート文字列の動作テスト

--- .github/github-script.js
+++ .github/github-script.js
@@ -3,6 +3,9 @@ module.exports = async ({ github, context }) => {
     owner: context.repo.owner,
     repo: context.repo.repo,
     commit_sha: context.payload.pull_request.head.sha,
-    body: "👋 Thanks for reporting!",
+    body: `
+      👋 Thanks for reporting!
+      <image src="https://github.com/speee/marp-speee-theme/raw/1ed497697d73bb5b56820ac07a55272096a3d7c9/example/example.001.png" />
+    `,
   });
 };
