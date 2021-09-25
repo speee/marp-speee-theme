05d4aef91e2c64b6a1bcaad825426a901107777a
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-09-25 10:57:03 +0900

    ボット書き込みを微調整

--- .github/github-script.js
+++ .github/github-script.js
@@ -4,10 +4,13 @@ module.exports = async ({ github, context }) => {
     repo: context.repo.repo,
     commit_sha: context.payload.pull_request.head.sha,
     body: `
-<details><summary>スライドのレンダリング結果</summary>
-<image src="https://github.com/${context.repo.owner}/${context.repo.repo}/raw/${context.sha}/example/example.001.png" />
-</details>
-<image src="https://github.com/${context.repo.owner}/${context.repo.repo}/raw/${context.sha}/example/example.002.png" />
+<details>
+
+<summary>スライドのレンダリング結果</summary>
+
+<img src="https://github.com/${context.repo.owner}/${context.repo.repo}/raw/${context.sha}/example/example.001.png" height="360px" />
+<img src="https://github.com/${context.repo.owner}/${context.repo.repo}/raw/${context.sha}/example/example.002.png" height="360px" />
+
 </details>
     `,
   });
